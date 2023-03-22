0c168f21a0549c588513e1d99a140c073ae94f81
Joshua Feingold <jfeingold35@gmail.com>
2023-03-22 15:05:04 -0500

    temporary. asdfasdfasdfasdfasfd

--- sfge/src/main/java/com/salesforce/rules/unusedmethod/InstanceMethodCallValidator.java
+++ sfge/src/main/java/com/salesforce/rules/unusedmethod/InstanceMethodCallValidator.java
@@ -1,17 +1,96 @@
 package com.salesforce.rules.unusedmethod;
 
+import com.salesforce.collections.CollectionUtil;
 import com.salesforce.graph.vertex.MethodCallExpressionVertex;
 import com.salesforce.graph.vertex.MethodVertex;
+import com.salesforce.graph.vertex.UserClassVertex;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Helper class for {@link com.salesforce.rules.UnusedMethodRule}. Used for determining whether an
  * instance method is ever invoked, e.g., {@code someMethod()}, {@code super.someMethod())}, etc.
  */
 public class InstanceMethodCallValidator extends BaseMethodCallValidator {
+    private final Set<String> potentialInternalCallers;
+    private final Set<String> thisableTypes;
+    private final Set<String> superableTypes;
+    private final Map<String, Set<String>> limitedExternalityMap;
+    private final Set<String> fullyExternalTypes;
+
+
+
     public InstanceMethodCallValidator(
             MethodVertex targetMethod, RuleStateTracker ruleStateTracker) {
         super(targetMethod, ruleStateTracker);
+        potentialInternalCallers = CollectionUtil.newTreeSet();
+        thisableTypes = CollectionUtil.newTreeSet();
+        superableTypes = CollectionUtil.newTreeSet();
+        limitedExternalityMap = CollectionUtil.newTreeMap();
+        fullyExternalTypes = CollectionUtil.newTreeSet();
+    }
+
+    private void identifyScopes() {
+        /*
+        - Private:
+            - This-ability:
+                - Self
+            - Super-ability:
+                - Nowhere
+            - Limited externality:
+                - Self => self, inner, outer, and siblings.
+        - Protected:
+            - This-ability:
+                - Per private
+                - Non-overriding subclasses
+            - Super-ability:
+                - Non-overriding subclasses
+                - Directly overriding subclasses
+            - Limited externality:
+                - Per private
+                - Any private this-able => self.
+                - Any public this-able => All subclasses.
+        - Public:
+            - This-ability:
+                - Per protected
+            - Super-ability:
+                - Per protected
+            - Limited externality:
+                - Per protected
+                - Any private this-able => self, inner, outer, siblings.
+            - Full externality:
+                - Any public this-able
+        - Override:
+            - This-ability
+                - Per applicable scope
+                - Overridden superclasses
+         */
+
+        // The target method is always this-able on its own defining type.
+        String ownDefiningType = targetMethod.getDefiningType();
+        thisableTypes.add(ownDefiningType);
+
+        // The method's defining type always grants limited externality to itself and its inner/outer/sibling classes.
+        Set<String> ownExternalitySet = CollectionUtil.newTreeSetOf(ownDefiningType);
+        List<UserClassVertex> typesInOwnFile = ruleStateTracker.getInnerClasses(ownDefiningType.split("\\.")[0]);
+        for (UserClassVertex typeInOwnFile : typesInOwnFile) {
+            ownExternalitySet.add(typeInOwnFile.getDefiningType());
+        }
+    }
+
+    private boolean internal() {
+        // Get every method call expression occurring in any potential internal caller.
+        List<MethodCallExpressionVertex> potentialCallers = ruleStateTracker.getMethodCallExpressionsByDefiningType()
+
+        return false;
+    }
+
+    private boolean external() {
+        return false;
     }
 
     /**
@@ -74,5 +153,38 @@ public class InstanceMethodCallValidator extends BaseMethodCallValidator {
     protected boolean externalUsageDetected() {
         // TODO: IMPLEMENT THIS METHOD.
         return false;
+    }
+
+    private List<UserClassVertex> getSuperclasses() {
+        return new ArrayList<>();
+    }
+
+    private class SubclassSummary {
+        private List<UserClassVertex> inheritingSubclasses;
+        private List<UserClassVertex> overridingSubclasses;
+        private List<UserClassVertex> miscellaneousSubclasses;
+
+
+        public SubclassSummary(String definingType) {
+            this.inheritingSubclasses = new ArrayList<>();
+            this.overridingSubclasses = new ArrayList<>();
+            this.miscellaneousSubclasses = new ArrayList<>();
+
+            // Only heritable methods require a meaningful summary of subclasses.
+            if (methodIsHeritable()) {
+            }
+        }
+
+        public List<UserClassVertex> getInheritingSubclasses() {
+            return inheritingSubclasses;
+        }
+
+        public List<UserClassVertex> getOverridingSubclasses() {
+            return overridingSubclasses;
+        }
+
+        public List<UserClassVertex> getMiscellaneousSubclasses() {
+            return miscellaneousSubclasses;
+        }
     }
 }
